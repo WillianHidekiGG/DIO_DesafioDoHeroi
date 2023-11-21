9a8021cb1161e5e741c7fcc399165d060601e0fb
WillianHideki <willianhidekigg@gmail.com>
2023-11-21 11:45:12 -0300

    feat:adição da tela final da aventura

--- confi.js
+++ confi.js
@@ -1,10 +1,9 @@
-var pontuacao = 10;
-
+var pontuacao = 0;
 var classificacoes = ["FERRO","BRONZE", "PRATA", "OURO", "PLATINA", "ASCENDENTAL", "INMORTAL", "RADIANTE"]
-
+var vidaVilao = 4;
 
 function confirmar(){
-    var nomes = document.getElementById("nome").value;
+var nomes = document.getElementById("nome").value;
 
     if(document.getElementById("nome").value == ""){
         alert("Informe um nome")
@@ -12,37 +11,88 @@ function confirmar(){
     document.getElementById("conteiner1").style.display = "none";
     document.getElementById("conteiner2").style.display = "block";
     document.getElementById("nomeSuper").innerHTML = nomes;
+    document.getElementById("classificacaoPontos").innerHTML = "NIVEL DO SEU HERÓI É: "  + classificacoes[0];
+    document.getElementById("pontuação").innerHTML ="SCORE: " + pontuacao;
     }
    
-    if(pontuacao <= 1000){
-     document.getElementById("classificacaoPontos").innerHTML = "NIVEL DO SEU HERÓI É: "  + classificacoes[0];
-     document.getElementById("pontuação").innerHTML ="SCORE: " + pontuacao;
-        }else if(pontuacao <= 2000){
-            document.getElementById("classificacaoPontos").innerHTML = "NIVEL DO SEU HERÓI É: "  + classificacoes[1];
-            document.getElementById("pontuação").innerHTML = "SCORE: " + pontuacao;
-        }else if(pontuacao <= 5000){
-            document.getElementById("classificacaoPontos").innerHTML = "NIVEL DO SEU HERÓI É: "  + classificacoes[2];
-            document.getElementById("pontuação").innerHTML = "SCORE: " + pontuacao;
-        }else if(pontuacao <= 7000){
-            document.getElementById("classificacaoPontos").innerHTML = "NIVEL DO SEU HERÓI É: "  + classificacoes[3];
-            document.getElementById("pontuação").innerHTML = "SCORE: " + pontuacao;
-        }else if(pontuacao <= 8000){
-            document.getElementById("classificacaoPontos").innerHTML = "NIVEL DO SEU HERÓI É: "  + classificacoes[4];
-            document.getElementById("pontuação").innerHTML = "SCORE: " + pontuacao;
-        }else if(pontuacao <= 9000){
-            document.getElementById("classificacaoPontos").innerHTML = "NIVEL DO SEU HERÓI É: "  + classificacoes[5];
-            document.getElementById("pontuação").innerHTML = "SCORE: " + pontuacao;
-        }else if(pontuacao <= 10000){
-            document.getElementById("classificacaoPontos").innerHTML = "NIVEL DO SEU HERÓI É: "  + classificacoes[6];
-            document.getElementById("pontuação").innerHTML = "SCORE: " + pontuacao;
-        }else if(pontuacao >= 10001){
-            document.getElementById("classificacaoPontos").innerHTML = "NIVEL DO SEU HERÓI É: "  + classificacoes[7];
-            document.getElementById("pontuação").innerHTML = "SCORE: " + pontuacao;
-        }else{
-            document.getElementById("classificacaoPontos").innerHTML = "NIVEL DO SEU HERÓI É: "  + classificacoes[0];
-            document.getElementById("pontuação").innerHTML = "SCORE: " + pontuacao;
-        }
+  
 }
 
 
+function atacar(){
+    document.getElementById("vidaVilao").innerHTML = vidaVilao;
+    vidaVilao--;
+    if (vidaVilao == -1){
+        document.getElementById("inimigo").style.display = "none";
+        document.getElementById("vidaVilao").innerHTML = "";
+        pontuacao = pontuacao+1000;
+        document.getElementById("pontuação").innerHTML ="SCORE: " + pontuacao;
+        vidaVilao = 4
+    }
+    if(pontuacao <= 1000){
+        document.getElementById("classificacaoPontos").innerHTML = "NIVEL DO SEU HERÓI É: "  + classificacoes[0];
+        document.getElementById("pontuação").innerHTML ="SCORE: " + pontuacao;
+           }else if(pontuacao <= 2000){
+               document.getElementById("classificacaoPontos").innerHTML = "NIVEL DO SEU HERÓI É: "  + classificacoes[1];
+               document.getElementById("pontuação").innerHTML = "SCORE: " + pontuacao;
+           }else if(pontuacao <= 5000){
+               document.getElementById("classificacaoPontos").innerHTML = "NIVEL DO SEU HERÓI É: "  + classificacoes[2];
+               document.getElementById("pontuação").innerHTML = "SCORE: " + pontuacao;
+           }else if(pontuacao <= 7000){
+               document.getElementById("classificacaoPontos").innerHTML = "NIVEL DO SEU HERÓI É: "  + classificacoes[3];
+               document.getElementById("pontuação").innerHTML = "SCORE: " + pontuacao;
+           }else if(pontuacao <= 8000){
+               document.getElementById("classificacaoPontos").innerHTML = "NIVEL DO SEU HERÓI É: "  + classificacoes[4];
+               document.getElementById("pontuação").innerHTML = "SCORE: " + pontuacao;
+           }else if(pontuacao <= 9000){
+               document.getElementById("classificacaoPontos").innerHTML = "NIVEL DO SEU HERÓI É: "  + classificacoes[5];
+               document.getElementById("pontuação").innerHTML = "SCORE: " + pontuacao;
+           }else if(pontuacao <= 10000){
+               document.getElementById("classificacaoPontos").innerHTML = "NIVEL DO SEU HERÓI É: "  + classificacoes[6];
+               document.getElementById("pontuação").innerHTML = "SCORE: " + pontuacao;
+           }else if(pontuacao >= 10001){
+               document.getElementById("classificacaoPontos").innerHTML = "NIVEL DO SEU HERÓI É: "  + classificacoes[7];
+               document.getElementById("pontuação").innerHTML = "SCORE: " + pontuacao;
+           }else{
+               document.getElementById("classificacaoPontos").innerHTML = "NIVEL DO SEU HERÓI É: "  + classificacoes[0];
+               document.getElementById("pontuação").innerHTML = "SCORE: " + pontuacao;
+           }
+}
 
+
+function fimAventura(){
+    var nomes = document.getElementById("nome").value;
+    document.getElementById("conteiner2").style.display = "none";
+    document.getElementById("container5").style.display = "block";
+    document.getElementById("parabens").innerHTML = "PARABENS PELA SUA AVENTURA " + nomes;
+    document.getElementById("classificacaoFinal").innerHTML = "NIVEL DO SEU HERÓI É: "  + classificacoes[0];
+    document.getElementById("pontosFinais").innerHTML ="SCORE: " + pontuacao;
+    if(pontuacao <= 1000){
+        document.getElementById("classificacaoFinal").innerHTML = "NIVEL DO SEU HERÓI É: "  + classificacoes[0];
+        document.getElementById("pontosFinais").innerHTML ="SCORE: " + pontuacao;
+           }else if(pontuacao <= 2000){
+               document.getElementById("classificacaoFinal").innerHTML = "NIVEL DO SEU HERÓI É: "  + classificacoes[1];
+               document.getElementById("pontosFinais").innerHTML = "SCORE: " + pontuacao;
+           }else if(pontuacao <= 5000){
+               document.getElementById("classificacaoFinal").innerHTML = "NIVEL DO SEU HERÓI É: "  + classificacoes[2];
+               document.getElementById("pontosFinais").innerHTML = "SCORE: " + pontuacao;
+           }else if(pontuacao <= 7000){
+               document.getElementById("classificacaoFinal").innerHTML = "NIVEL DO SEU HERÓI É: "  + classificacoes[3];
+               document.getElementById("pontosFinais").innerHTML = "SCORE: " + pontuacao;
+           }else if(pontuacao <= 8000){
+               document.getElementById("classificacaoFinal").innerHTML = "NIVEL DO SEU HERÓI É: "  + classificacoes[4];
+               document.getElementById("pontosFinais").innerHTML = "SCORE: " + pontuacao;
+           }else if(pontuacao <= 9000){
+               document.getElementById("classificacaoFinal").innerHTML = "NIVEL DO SEU HERÓI É: "  + classificacoes[5];
+               document.getElementById("pontosFinais").innerHTML = "SCORE: " + pontuacao;
+           }else if(pontuacao <= 10000){
+               document.getElementById("classificacaoFinal").innerHTML = "NIVEL DO SEU HERÓI É: "  + classificacoes[6];
+               document.getElementById("pontosFinais").innerHTML = "SCORE: " + pontuacao;
+           }else if(pontuacao >= 10001){
+               document.getElementById("classificacaoFinal").innerHTML = "NIVEL DO SEU HERÓI É: "  + classificacoes[7];
+               document.getElementById("pontosFinais").innerHTML = "SCORE: " + pontuacao;
+           }else{
+               document.getElementById("classificacaoFinal").innerHTML = "NIVEL DO SEU HERÓI É: "  + classificacoes[0];
+               document.getElementById("pontosFinais").innerHTML = "SCORE: " + pontuacao;
+           }
+}
