2bf6940969520a6be4773b765f0f44a652e61b84
WillianHideki <willianhidekigg@gmail.com>
2023-11-22 04:19:36 -0300

    feat:botão de reset

--- confi.js
+++ confi.js
@@ -11,10 +11,9 @@ var nomes = document.getElementById("nome").value;
     document.getElementById("conteiner1").style.display = "none";
     document.getElementById("conteiner2").style.display = "block";
     document.getElementById("nomeSuper").innerHTML = nomes;
-    document.getElementById("classificacaoPontos").innerHTML = "NIVEL DO SEU HERÓI É: "  + classificacoes[0];
-    document.getElementById("pontuação").innerHTML ="SCORE: " + pontuacao;
+    document.getElementById("classificacaoPontos").innerHTML = "A DIFICULDADE DA SUA AVENTURA É: "  + classificacoes[0];
+    document.getElementById("pontuação").innerHTML ="EXPERIÊNCIA: " + pontuacao;
     }
-   
   
 }
 
@@ -26,36 +25,36 @@ function atacar(){
         document.getElementById("inimigo").style.display = "none";
         document.getElementById("vidaVilao").innerHTML = "";
         pontuacao = pontuacao+1000;
-        document.getElementById("pontuação").innerHTML ="SCORE: " + pontuacao;
+        document.getElementById("pontuação").innerHTML ="EXPERIÊNCIA: " + pontuacao;
         vidaVilao = 4
     }
     if(pontuacao <= 1000){
-        document.getElementById("classificacaoPontos").innerHTML = "NIVEL DO SEU HERÓI É: "  + classificacoes[0];
-        document.getElementById("pontuação").innerHTML ="SCORE: " + pontuacao;
+        document.getElementById("classificacaoPontos").innerHTML = "A DIFICULDADE DA SUA AVENTURA É: "  + classificacoes[0];
+        document.getElementById("pontuação").innerHTML ="EXPERIÊNCIA: " + pontuacao;
            }else if(pontuacao <= 2000){
-               document.getElementById("classificacaoPontos").innerHTML = "NIVEL DO SEU HERÓI É: "  + classificacoes[1];
-               document.getElementById("pontuação").innerHTML = "SCORE: " + pontuacao;
+               document.getElementById("classificacaoPontos").innerHTML = "A DIFICULDADE DA SUA AVENTURA É: "  + classificacoes[1];
+               document.getElementById("pontuação").innerHTML = "EXPERIÊNCIA: " + pontuacao;
            }else if(pontuacao <= 5000){
-               document.getElementById("classificacaoPontos").innerHTML = "NIVEL DO SEU HERÓI É: "  + classificacoes[2];
-               document.getElementById("pontuação").innerHTML = "SCORE: " + pontuacao;
+               document.getElementById("classificacaoPontos").innerHTML = "A DIFICULDADE DA SUA AVENTURA É: "  + classificacoes[2];
+               document.getElementById("pontuação").innerHTML = "EXPERIÊNCIA: " + pontuacao;
            }else if(pontuacao <= 7000){
-               document.getElementById("classificacaoPontos").innerHTML = "NIVEL DO SEU HERÓI É: "  + classificacoes[3];
-               document.getElementById("pontuação").innerHTML = "SCORE: " + pontuacao;
+               document.getElementById("classificacaoPontos").innerHTML = "A DIFICULDADE DA SUA AVENTURA É: "  + classificacoes[3];
+               document.getElementById("pontuação").innerHTML = "EXPERIÊNCIA: " + pontuacao;
            }else if(pontuacao <= 8000){
-               document.getElementById("classificacaoPontos").innerHTML = "NIVEL DO SEU HERÓI É: "  + classificacoes[4];
-               document.getElementById("pontuação").innerHTML = "SCORE: " + pontuacao;
+               document.getElementById("classificacaoPontos").innerHTML = "A DIFICULDADE DA SUA AVENTURA É: "  + classificacoes[4];
+               document.getElementById("pontuação").innerHTML = "EXPERIÊNCIA: " + pontuacao;
            }else if(pontuacao <= 9000){
-               document.getElementById("classificacaoPontos").innerHTML = "NIVEL DO SEU HERÓI É: "  + classificacoes[5];
-               document.getElementById("pontuação").innerHTML = "SCORE: " + pontuacao;
+               document.getElementById("classificacaoPontos").innerHTML = "A DIFICULDADE DA SUA AVENTURA É: "  + classificacoes[5];
+               document.getElementById("pontuação").innerHTML = "EXPERIÊNCIA: " + pontuacao;
            }else if(pontuacao <= 10000){
-               document.getElementById("classificacaoPontos").innerHTML = "NIVEL DO SEU HERÓI É: "  + classificacoes[6];
-               document.getElementById("pontuação").innerHTML = "SCORE: " + pontuacao;
+               document.getElementById("classificacaoPontos").innerHTML = "A DIFICULDADE DA SUA AVENTURA É: "  + classificacoes[6];
+               document.getElementById("pontuação").innerHTML = "EXPERIÊNCIA: " + pontuacao;
            }else if(pontuacao >= 10001){
-               document.getElementById("classificacaoPontos").innerHTML = "NIVEL DO SEU HERÓI É: "  + classificacoes[7];
-               document.getElementById("pontuação").innerHTML = "SCORE: " + pontuacao;
+               document.getElementById("classificacaoPontos").innerHTML = "A DIFICULDADE DA SUA AVENTURA É: "  + classificacoes[7];
+               document.getElementById("pontuação").innerHTML = "EXPERIÊNCIA: " + pontuacao;
            }else{
-               document.getElementById("classificacaoPontos").innerHTML = "NIVEL DO SEU HERÓI É: "  + classificacoes[0];
-               document.getElementById("pontuação").innerHTML = "SCORE: " + pontuacao;
+               document.getElementById("classificacaoPontos").innerHTML = "A DIFICULDADE DA SUA AVENTURA É: "  + classificacoes[0];
+               document.getElementById("pontuação").innerHTML = "EXPERIÊNCIA: " + pontuacao;
            }
 }
 
@@ -65,34 +64,39 @@ function fimAventura(){
     document.getElementById("conteiner2").style.display = "none";
     document.getElementById("container5").style.display = "block";
     document.getElementById("parabens").innerHTML = "PARABENS PELA SUA AVENTURA " + nomes;
-    document.getElementById("classificacaoFinal").innerHTML = "NIVEL DO SEU HERÓI É: "  + classificacoes[0];
-    document.getElementById("pontosFinais").innerHTML ="SCORE: " + pontuacao;
+    document.getElementById("classificacaoFinal").innerHTML = "A DIFICULDADE DA SUA AVENTURA É: "  + classificacoes[0];
+    document.getElementById("pontosFinais").innerHTML ="EXPERIÊNCIA: " + pontuacao;
     if(pontuacao <= 1000){
-        document.getElementById("classificacaoFinal").innerHTML = "NIVEL DO SEU HERÓI É: "  + classificacoes[0];
-        document.getElementById("pontosFinais").innerHTML ="SCORE: " + pontuacao;
+        document.getElementById("classificacaoFinal").innerHTML = "A DIFICULDADE DA SUA AVENTURA É: "  + classificacoes[0];
+        document.getElementById("pontosFinais").innerHTML ="EXPERIÊNCIA: " + pontuacao;
            }else if(pontuacao <= 2000){
-               document.getElementById("classificacaoFinal").innerHTML = "NIVEL DO SEU HERÓI É: "  + classificacoes[1];
+               document.getElementById("classificacaoFinal").innerHTML = "A DIFICULDADE DA SUA AVENTURA É: "  + classificacoes[1];
                document.getElementById("pontosFinais").innerHTML = "SCORE: " + pontuacao;
            }else if(pontuacao <= 5000){
-               document.getElementById("classificacaoFinal").innerHTML = "NIVEL DO SEU HERÓI É: "  + classificacoes[2];
-               document.getElementById("pontosFinais").innerHTML = "SCORE: " + pontuacao;
+               document.getElementById("classificacaoFinal").innerHTML = "A DIFICULDADE DA SUA AVENTURA É: "  + classificacoes[2];
+               document.getElementById("pontosFinais").innerHTML = "EXPERIÊNCIA: " + pontuacao;
            }else if(pontuacao <= 7000){
-               document.getElementById("classificacaoFinal").innerHTML = "NIVEL DO SEU HERÓI É: "  + classificacoes[3];
-               document.getElementById("pontosFinais").innerHTML = "SCORE: " + pontuacao;
+               document.getElementById("classificacaoFinal").innerHTML = "A DIFICULDADE DA SUA AVENTURA É: "  + classificacoes[3];
+               document.getElementById("pontosFinais").innerHTML = "EXPERIÊNCIA: " + pontuacao;
            }else if(pontuacao <= 8000){
-               document.getElementById("classificacaoFinal").innerHTML = "NIVEL DO SEU HERÓI É: "  + classificacoes[4];
-               document.getElementById("pontosFinais").innerHTML = "SCORE: " + pontuacao;
+               document.getElementById("classificacaoFinal").innerHTML = "A DIFICULDADE DA SUA AVENTURA É: "  + classificacoes[4];
+               document.getElementById("pontosFinais").innerHTML = "EXPERIÊNCIA: " + pontuacao;
            }else if(pontuacao <= 9000){
-               document.getElementById("classificacaoFinal").innerHTML = "NIVEL DO SEU HERÓI É: "  + classificacoes[5];
-               document.getElementById("pontosFinais").innerHTML = "SCORE: " + pontuacao;
+               document.getElementById("classificacaoFinal").innerHTML = "A DIFICULDADE DA SUA AVENTURA É: "  + classificacoes[5];
+               document.getElementById("pontosFinais").innerHTML = "EXPERIÊNCIA: " + pontuacao;
            }else if(pontuacao <= 10000){
-               document.getElementById("classificacaoFinal").innerHTML = "NIVEL DO SEU HERÓI É: "  + classificacoes[6];
-               document.getElementById("pontosFinais").innerHTML = "SCORE: " + pontuacao;
+               document.getElementById("classificacaoFinal").innerHTML = "A DIFICULDADE DA SUA AVENTURA É: "  + classificacoes[6];
+               document.getElementById("pontosFinais").innerHTML = "EXPERIÊNCIA: " + pontuacao;
            }else if(pontuacao >= 10001){
-               document.getElementById("classificacaoFinal").innerHTML = "NIVEL DO SEU HERÓI É: "  + classificacoes[7];
-               document.getElementById("pontosFinais").innerHTML = "SCORE: " + pontuacao;
+               document.getElementById("classificacaoFinal").innerHTML = "A DIFICULDADE DA SUA AVENTURA É: "  + classificacoes[7];
+               document.getElementById("pontosFinais").innerHTML = "EXPERIÊNCIA: " + pontuacao;
            }else{
-               document.getElementById("classificacaoFinal").innerHTML = "NIVEL DO SEU HERÓI É: "  + classificacoes[0];
-               document.getElementById("pontosFinais").innerHTML = "SCORE: " + pontuacao;
+               document.getElementById("classificacaoFinal").innerHTML = "A DIFICULDADE DA SUA AVENTURA É: "  + classificacoes[0];
+               document.getElementById("pontosFinais").innerHTML = "EXPERIÊNCIA: " + pontuacao;
            }
+          
 }
+
+function recarregarAPagina(){
+    window.location.reload();
+} 
